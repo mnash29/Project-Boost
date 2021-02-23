a0633a1eba2cbfc0188fa0c27605d890fbcd314a
Matthew Nash <mtntrades10@gmail.com>
2021-02-23 13:40:32 -0800

    Added more scenes and applied invoke delay on crash and success

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,28 +3,57 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float invokeDelay = 1f;
     void OnCollisionEnter(Collision other)
     {
+
         switch (other.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("Bumped into a friendly object");
                 break;
             case "Finish":
-                Debug.Log("You landed on the Landing Pad!");
+                StartSuccessSequence();
                 break;
             case "Fuel":
                 Debug.Log("You re-fueled! VROOOOSH!");
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
+    }
+
+    void StartSuccessSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", invokeDelay);
+    }
+    
+    void StartCrashSequence()
+    {
+        // TODO add crash audio SFX
+        // TODO add particle effect upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", invokeDelay);
     }
 
     void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextlevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
